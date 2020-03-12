447e5b22f8475dc01b42dbf04f9302393759bc75
Anil Thomson <anil.thomson@nordstrom.com>
2020-03-12 10:35:19 -0700

    adding more

---
+++ TOH.java
@@ -0,0 +1,53 @@
+
+
+public class TOH {
+	static int x = 1;
+	static char[] splitters = { '0', '@', '#', '$', '%', '&', '*' };
+
+	// Java recursive function to solve tower of hanoi puzzle
+	static void towerOfHanoi(int n, String from_rod, String to_rod, String aux_rod) {
+		printStart(n, n);
+		if (n == 1) {
+			print("Move disk 1 from rod " + from_rod + " to rod " + to_rod + "   " + n, n);
+			printEnd(n, n);
+			return;
+		}
+		towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+		print("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod + "   " + n, n);
+		towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+		printEnd(n, n);
+	}
+
+	// Driver method
+	public static void main(String args[]) {
+		long l = System.currentTimeMillis();
+		int n = 4; // Number of disks
+		towerOfHanoi(n, "A", "C", "B"); // A, B and C are names of rods
+		System.out.println(System.currentTimeMillis() - l);
+	}
+
+	private static void print(String s, int space) {
+		for (int i = 0; i < 10/space; i++)
+			System.out.print(" ");
+		System.out.println(s);
+		x++;
+	}
+
+	private static void printEnd(int n, int space) {
+		for (int i = 0; i < 10/space; i++)
+			System.out.print(" ");
+		System.out.print("Ending " + n + "  ");
+	//	for (int i = 0; i < 10; i++)
+	//		System.out.print(splitters[n]);
+		System.out.println();
+	}
+
+	private static void printStart(int n, int space) {
+		for (int i = 0; i < 10/space; i++)
+			System.out.print(" ");
+		System.out.print("Starting " + n + "  ");
+	//	for (int i = 0; i < 10; i++)
+	//		System.out.print(splitters[n]);
+		System.out.println();
+	}
+}
---
+++ Tree.java
@@ -0,0 +1,85 @@
+
+
+public class Tree {
+	static class Node {
+		int data;
+		Node left;
+		Node right;
+
+		Node(int input) {
+			data = input;
+		}
+	}
+
+	public static void inorder(Node node) { // ^
+		if (node == null)
+			return;
+		Node left = node.left;
+		Node right = node.right;
+		inorder(left);
+		System.out.print(node.data + " ");
+		inorder(right);
+	}
+
+	public static void postorder(Node node) { // >
+		if (node == null)
+			return;
+		Node left = node.left;
+		Node right = node.right;
+		postorder(left);
+		postorder(right);
+		System.out.print(node.data + " ");
+
+	}
+
+	public static void preorder(Node node) { // <
+		if (node == null)
+			return;
+		Node left = node.left;
+		Node right = node.right;
+		System.out.print(node.data + " ");
+		preorder(left);
+		preorder(right);
+
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(25);
+		Node left = new Node(15);
+		Node right = new Node(50);
+		root.left = left;
+		root.right = right;
+		Node lnode = new Node(10);
+		Node lnode2 = new Node(22);
+		left.left = lnode;
+		left.right = lnode2;
+		Node rnode = new Node(35);
+		Node rnode2 = new Node(70);
+		 right.left = rnode;
+		right.right = rnode2;
+		 Node llnode = new Node(4);
+		Node lrnode2 = new Node(12);
+		lnode.left = llnode;
+		lnode.right = lrnode2;
+		 
+		Node llnodexy = new Node(18);
+		Node lrnode2xy = new Node(24);
+		lnode2.left = llnodexy;
+		lnode2.right = lrnode2xy;
+		
+		Node rlnode = new Node(31);
+		Node rrnode2 = new Node(44);
+		rnode.left = rlnode;
+		rnode.right = rrnode2;
+		Node rlnodex = new Node(66);
+		Node rrnode2x = new Node(90);
+		rnode2.left = rlnodex;
+		rnode2.right = rrnode2x;
+	  
+		inorder(root);
+		System.out.println();
+		preorder(root);
+		System.out.println();
+		postorder(root);
+	}
+}

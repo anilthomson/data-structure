faf47fe91616acda5fb4eb69d387af5bcdb0c2bb
Anil Thomson <anil.thomson@nordstrom.com>
2020-03-17 09:36:36 -0700

    more files

---
+++ AddLinkListNumbers.java
@@ -0,0 +1,30 @@
+public class AddLinkListNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addTwoNumbers(l1, l2, 0);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2, int carry) {
+        ListNode node = new ListNode(0);
+        if (l1 == null && l2 == null && carry == 0)
+            return null;
+        else if (l1 == null && l2 == null) {
+            node.val = carry;
+        } else if (l1 != null && l2 != null) {
+            int sum = l1.val + l2.val + carry;
+            int result = sum % 10;
+            node.val = result;
+            node.next = addTwoNumbers(l1.next, l2.next, sum / 10);
+        } else if (l1 != null) {
+            int sum = l1.val + carry;
+            int result = sum % 10;
+            node.val = result;
+            node.next = addTwoNumbers(l1.next, l2, sum / 10);
+        } else {
+            int sum = l2.val + carry;
+            int result = sum % 10;
+            node.val = result;
+            node.next = addTwoNumbers(l1, l2.next, sum / 10);
+        }
+        return node;
+    }
+}
---
+++ SortLinkedList.java
@@ -0,0 +1,117 @@
+public class SortLinkedList { 
+    Node head = null; 
+    // Node a, b; 
+    static class Node { 
+        int val; 
+        Node next; 
+  
+        public Node(int val) 
+        { 
+            this.val = val; 
+        } 
+    } 
+  
+    Node sortedMerge(Node a, Node b) 
+    { 
+        Node result = null; 
+        /* Base cases */
+        if (a == null) 
+            return b; 
+        if (b == null) 
+            return a; 
+  
+        /* Pick either a or b, and recur */
+        if (a.val <= b.val) { 
+            result = a; 
+            result.next = sortedMerge(a.next, b); 
+        } 
+        else { 
+            result = b; 
+            result.next = sortedMerge(a, b.next); 
+        } 
+        return result; 
+    } 
+  
+    Node mergeSort(Node h) 
+    { 
+        // Base case : if head is null 
+        if (h == null || h.next == null) { 
+            return h; 
+        } 
+  
+        // get the middle of the list 
+        Node middle = getMiddle(h); 
+        Node nextofmiddle = middle.next; 
+  
+        // set the next of middle Node to null 
+        middle.next = null; 
+  
+        // Apply mergeSort on left list 
+        Node left = mergeSort(h); 
+  
+        // Apply mergeSort on right list 
+        Node right = mergeSort(nextofmiddle); 
+  
+        // Merge the left and right lists 
+        Node sortedlist = sortedMerge(left, right); 
+        return sortedlist; 
+    } 
+  
+    // Utility function to get the middle of the linked list 
+    public static Node getMiddle(Node head) 
+    { 
+        if (head == null) 
+            return head; 
+  
+        Node slow = head, fast = head; 
+  
+        while (fast.next != null && fast.next.next != null) { 
+            slow = slow.next; 
+            fast = fast.next.next; 
+        } 
+        return slow; 
+    } 
+  
+    void push(int new_data) 
+    { 
+        /* allocate Node */
+        Node new_Node = new Node(new_data); 
+  
+        /* link the old list off the new Node */
+        new_Node.next = head; 
+  
+        /* move the head to point to the new Node */
+        head = new_Node; 
+    } 
+  
+    // Utility function to print the linked list 
+    void printList(Node headref) 
+    { 
+        while (headref != null) { 
+            System.out.print(headref.val + " "); 
+            headref = headref.next; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+  
+        SortLinkedList li = new SortLinkedList(); 
+        /* 
+         * Let us create a unsorted linked list to test the functions 
+         * created. The list shall be a: 2->3->20->5->10->15 
+         */
+        li.push(15); 
+        li.push(10); 
+        li.push(5); 
+        li.push(20); 
+        li.push(3); 
+        li.push(2); 
+  
+        // Apply merge Sort 
+        li.head = li.mergeSort(li.head); 
+        System.out.print("\n Sorted Linked List is: \n"); 
+        li.printList(li.head); 
+    } 
+} 
+  
